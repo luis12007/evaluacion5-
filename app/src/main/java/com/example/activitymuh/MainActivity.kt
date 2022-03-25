17558198c6be5aa54c4d421c9b3cef673a4e62e4
Luis Alexander Hernandez Martinez <00129020@uca.edu.sv>
2022-03-24 18:10:15 -0600

    al_fin_:_(

--- app/src/main/java/com/example/activitymuh/MainActivity.kt
+++ app/src/main/java/com/example/activitymuh/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.activitymuh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,13 +11,11 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     //data
-    private var text_number = 0
-    private var text_clicks = 0
-    private lateinit var text_change: TextView
-
-    //views
+    private var textNumber = 0
+    private var textClicks = 0
+    private lateinit var textChange: TextView
     //text
-    private lateinit var teamAScoreTextView: TextView
+    private lateinit var textCounting: TextView
     // Images
     private lateinit var imageFive: ImageView
     private lateinit var imageTwo: ImageView
@@ -24,135 +23,185 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imageOne: ImageView
     private lateinit var imageThree: ImageView
     //Actions
-    private lateinit var Action_button_back: Button
-    private lateinit var Action_buton_next: Button
+    private lateinit var actionButtonBack: Button
+    private lateinit var actionButtonNext: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Binder()
+        binder()
         addListeners()
+        changeImage(textClicks)
     }
 
     private fun addListeners() {
-        Action_buton_next.setOnClickListener {
-            text_number++
-            text_clicks++
-            VisualNumber(teamAScoreTextView, text_number)
-            changeImage(text_clicks)
+        actionButtonNext.setOnClickListener {
+            if(textClicks <= 15) {
+                textClicks++
+                visualnumber(textCounting, textNumber)
+                changeImage(textClicks)
+            }
         }
 
-         Action_button_back.setOnClickListener {
-            text_number--
-         VisualNumber(teamAScoreTextView, text_number)
-             changeImage(text_clicks)
+         actionButtonBack.setOnClickListener {
+             if (textClicks >= 0){
+                 textClicks--
+         visualnumber(textCounting, textNumber)
+             changeImage(textClicks)
+             }
          }
         }
 
-    private fun Binder(){
+    private fun binder(){
         // Btn
-        Action_button_back = findViewById(R.id.Action_BtnBlack)
-        Action_buton_next = findViewById(R.id.Action_BtnNext)
+        actionButtonBack = findViewById(R.id.Action_BtnBlack)
+        actionButtonNext = findViewById(R.id.Action_BtnNext)
         //text
-        teamAScoreTextView = findViewById(R.id.textViewCounting)
-
-    }
-
-
-    private fun VisualNumber(view: TextView, score: Int) {
-    view.text = score.toString()
-    }
-
-    private fun changeImage(click: Int) {
+        textCounting = findViewById(R.id.textViewCounting)
+        textChange = findViewById(R.id.textViewAction)
+        //img
         imageFive = findViewById(R.id.imageViewFive)
         imageTwo = findViewById(R.id.imageViewTwo)
         imageFour = findViewById(R.id.imageViewFour)
         imageOne = findViewById(R.id.imageViewOne)
         imageThree = findViewById(R.id.imageViewThree)
-        text_change = findViewById(R.id.textViewAction)
+
+    }
+
+    private fun visualnumber(view: TextView, score: Int) {
+    view.text = score.toString()
+    }
+
+    private fun changeImage(click: Int) {
 
         when(click) {
-            1-> {
-                    imageFive.setImageResource(R.drawable.cinco_comparado)
-                    imageTwo.setImageResource(R.drawable.dos_comparado)
-                    text_change.text = getString(R.string.text_subtitle_compare)
+            0 ->{
+                imageFive.setImageResource(R.drawable.cinco_normal)
+                imageTwo.setImageResource(R.drawable.dos_normal)
+                imageFour.setImageResource(R.drawable.cuatro_normal)
+                imageOne.setImageResource(R.drawable.uno_normal)
+                imageThree.setImageResource(R.drawable.tres_normal)
+                textChange.text = getString(R.string.text_subtitle_action)
+                textCounting.text = getString(R.string.cero)
+                actionButtonBack.visibility = View.INVISIBLE
             }
-            2-> {
-                    imageFive.setImageResource(R.drawable.dos_intercambio)
-                    imageTwo.setImageResource(R.drawable.cinco_comparado)
-                    text_change.text = getString(R.string.exchange)
-                }
-            3-> {
-                    imageFive.setImageResource(R.drawable.dos_normal)
-                    imageFour.setImageResource(R.drawable.cuatro_comparado)
-                    text_change.text = getString(R.string.text_subtitle_compare)
-                }
-            4-> {
-                    imageTwo.setImageResource(R.drawable.cuatro_intercambio)
-                    imageFour.setImageResource(R.drawable.cinco_comparado)
-                    text_change.text = getString(R.string.exchange)
-                }
-            5-> {
-                    imageTwo.setImageResource(R.drawable.cuatro_normal)
-                    imageOne.setImageResource(R.drawable.uno_comparado)
-                    text_change.text = getString(R.string.text_subtitle_compare)
-                }
-            6-> {
-                    imageFour.setImageResource(R.drawable.uno_intercambio)
-                    imageOne.setImageResource(R.drawable.cinco_comparado)
-                    text_change.text = getString(R.string.exchange)
-                }
-            7-> {
-                    imageFour.setImageResource(R.drawable.uno_normal)
-                    imageThree.setImageResource(R.drawable.tres_comparado)
-                    text_change.text = getString(R.string.text_subtitle_compare)
-                }
-            8-> {
-                    imageOne.setImageResource(R.drawable.tres_intercambio)
-                    imageThree.setImageResource(R.drawable.cinco_comparado)
-                    text_change.text = getString(R.string.exchange)
-                }
-            9-> {
-                    imageFive.setImageResource(R.drawable.dos_comparado)
-                    imageTwo.setImageResource(R.drawable.cuatro_comparado)
-                    imageOne.setImageResource(R.drawable.tres_normal)
-                    imageThree.setImageResource(R.drawable.cinco_normal)
-                    text_change.text = getString(R.string.text_subtitle_compare)
-                }
-            10-> {
-                    imageFive.setImageResource(R.drawable.dos_normal)
-                    imageFour.setImageResource(R.drawable.uno_comparado)
-                    text_change.text = getString(R.string.text_subtitle_compare)
-                }
-            11-> {
-                    imageTwo.setImageResource(R.drawable.uno_intercambio)
-                    imageFour.setImageResource(R.drawable.cuatro_comparado)
-                text_change.text = getString(R.string.exchange)
-                }
-            12-> {
-                    imageTwo.setImageResource(R.drawable.uno_normal)
-                    imageFour.setImageResource(R.drawable.cuatro_comparado)
-                    imageOne.setImageResource(R.drawable.tres_comparado)
-                    text_change.text = getString(R.string.text_subtitle_compare)
-                }
-            13-> {
-                    imageFour.setImageResource(R.drawable.tres_intercambio)
-                    imageOne.setImageResource(R.drawable.cuatro_comparado)
-                    text_change.text = getString(R.string.exchange)
-                }
-            14-> {
-                    imageFour.setImageResource(R.drawable.tres_normal)
-                    imageOne.setImageResource(R.drawable.cuatro_normal)
-                    imageFive.setImageResource(R.drawable.dos_comparado)
-                    imageTwo.setImageResource(R.drawable.uno_comparado)
-                    text_change.text = getString(R.string.text_subtitle_compare)
-                }
-            15-> {
-                    imageFive.setImageResource(R.drawable.uno_intercambio)
-                    imageTwo.setImageResource(R.drawable.dos_comparado)
-                    text_change.text = getString(R.string.exchange)
-                }
-        }
+            1 -> {
+                imageFive.setImageResource(R.drawable.cinco_comparado)
+                imageTwo.setImageResource(R.drawable.dos_comparado)
+                imageFour.setImageResource(R.drawable.cuatro_normal)
+                imageOne.setImageResource(R.drawable.uno_normal)
+                imageThree.setImageResource(R.drawable.tres_normal)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.one)
+                actionButtonBack.visibility = View.VISIBLE
+            }
+            2 -> {
+                imageFive.setImageResource(R.drawable.dos_intercambio)
+                imageTwo.setImageResource(R.drawable.cinco_comparado)
+                imageFour.setImageResource(R.drawable.cuatro_normal)
+                textChange.text = getString(R.string.exchange)
+                textCounting.text = getString(R.string.one)
+            }
+            3 -> {
+                imageFive.setImageResource(R.drawable.dos_normal)
+                imageFour.setImageResource(R.drawable.cuatro_comparado)
+                imageTwo.setImageResource(R.drawable.cinco_comparado)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.one)
+            }
+            4 -> {
+                imageTwo.setImageResource(R.drawable.cuatro_intercambio)
+                imageFour.setImageResource(R.drawable.cinco_comparado)
+                imageOne.setImageResource(R.drawable.uno_normal)
+                textChange.text = getString(R.string.exchange)
+                textCounting.text = getString(R.string.one)
+            }
+            5 -> {
+                imageTwo.setImageResource(R.drawable.cuatro_normal)
+                imageOne.setImageResource(R.drawable.uno_comparado)
+                imageFour.setImageResource(R.drawable.cinco_comparado)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.one)
+            }
+            6 -> {
+                imageFour.setImageResource(R.drawable.uno_intercambio)
+                imageOne.setImageResource(R.drawable.cinco_comparado)
+                imageThree.setImageResource(R.drawable.tres_normal)
+                textChange.text = getString(R.string.exchange)
+                textCounting.text = getString(R.string.one)
+            }
+            7 -> {
+                imageFour.setImageResource(R.drawable.uno_normal)
+                imageOne.setImageResource(R.drawable.cinco_comparado)
+                imageThree.setImageResource(R.drawable.tres_comparado)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.one)
+            }
+            8 -> {
+                imageOne.setImageResource(R.drawable.tres_intercambio)
+                imageThree.setImageResource(R.drawable.cinco_comparado)
+                imageFive.setImageResource(R.drawable.dos_normal)
+                imageTwo.setImageResource(R.drawable.cuatro_normal)
+                imageFour.setImageResource(R.drawable.uno_normal)
+                textChange.text = getString(R.string.exchange)
+                textCounting.text = getString(R.string.one)
+            }
+            9 -> {
+                imageFive.setImageResource(R.drawable.dos_comparado)
+                imageTwo.setImageResource(R.drawable.cuatro_comparado)
+                imageOne.setImageResource(R.drawable.tres_normal)
+                imageThree.setImageResource(R.drawable.cinco_normal)
+                imageFour.setImageResource(R.drawable.uno_normal)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.two)
+            }
+            10 -> {
+                imageFive.setImageResource(R.drawable.dos_normal)
+                imageTwo.setImageResource(R.drawable.cuatro_comparado)
+                imageFour.setImageResource(R.drawable.uno_comparado)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.two)
+            }
+            11 -> {
+                imageTwo.setImageResource(R.drawable.uno_intercambio)
+                imageFour.setImageResource(R.drawable.cuatro_comparado)
+                imageOne.setImageResource(R.drawable.tres_normal)
+                textChange.text = getString(R.string.exchange)
+                textCounting.text = getString(R.string.two)
+            }
+            12 -> {
+                imageTwo.setImageResource(R.drawable.uno_normal)
+                imageFour.setImageResource(R.drawable.cuatro_comparado)
+                imageOne.setImageResource(R.drawable.tres_comparado)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.two)
+            }
+            13 -> {
+                imageFour.setImageResource(R.drawable.tres_intercambio)
+                imageOne.setImageResource(R.drawable.cuatro_comparado)
+                imageFive.setImageResource(R.drawable.dos_normal)
+                imageTwo.setImageResource(R.drawable.uno_normal)
 
+                textChange.text = getString(R.string.exchange)
+                textCounting.text = getString(R.string.two)
+            }
+            14 -> {
+                imageFour.setImageResource(R.drawable.tres_normal)
+                imageOne.setImageResource(R.drawable.cuatro_normal)
+                imageFive.setImageResource(R.drawable.dos_comparado)
+                imageTwo.setImageResource(R.drawable.uno_comparado)
+                textChange.text = getString(R.string.text_subtitle_compare)
+                textCounting.text = getString(R.string.three)
+                actionButtonNext.visibility = View.VISIBLE
+
+            }
+            15 -> {
+                imageFive.setImageResource(R.drawable.uno_intercambio)
+                imageTwo.setImageResource(R.drawable.dos_comparado)
+                textChange.text = getString(R.string.exchange)
+                textCounting.text = getString(R.string.three)
+                actionButtonNext.visibility = View.INVISIBLE
+            }
+        }
     }
 }
